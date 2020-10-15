8cd647531bab03a5cf0b64544fb18b6a3e9804e2
alexmosunov <alexmosunov@gmail.com>
2020-10-15 17:49:57 +0300

    Initial Commit

---
+++ pet-sheltersTests/pet_sheltersTests.swift
@@ -0,0 +1,33 @@
+//
+//  pet_sheltersTests.swift
+//  pet-sheltersTests
+//
+//  Created by iOS on 15.10.2020.
+//
+
+import XCTest
+@testable import pet_shelters
+
+class pet_sheltersTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
